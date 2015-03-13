da1a5fd0a812c7f8c9d32c5d7c8a79c2d089ea8d
Leonardo Gandini <neko.gandini@gmail.com>
2015-03-13 15:42:03 +0000

    fixes

--- mobile/src/main/java/com/leonardogandini/circuswatchfaces/CircusMain.java
+++ mobile/src/main/java/com/leonardogandini/circuswatchfaces/CircusMain.java
@@ -1,26 +1,31 @@
 package com.leonardogandini.circuswatchfaces;
 
-        import android.support.v7.app.ActionBarActivity;
-        import android.os.Bundle;
-        import android.view.Menu;
-        import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-        import android.content.Intent;
-        import android.net.Uri;
-        import android.view.View;
-        import android.widget.Button;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import android.widget.Button;
 
-        import com.google.android.gms.ads.AdRequest;
-        import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 
 public class CircusMain extends ActionBarActivity {
 
     public void buttonOnClick(View v){
-        Uri uri = Uri.parse("http://tamrielma.ps");
+        Uri uri = Uri.parse("https://leonardogandini.com");
         Intent intent = new Intent(Intent.ACTION_VIEW, uri);
         startActivity(intent);
     }
-
+    public void buttonOnClickroberta(View v){
+        Uri uri = Uri.parse("http://robertapagnoni.it");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
